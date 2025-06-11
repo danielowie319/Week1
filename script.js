c35c662bd3b650588a619367eb018ec25333c2f5
danielowie319 <danielowie@gmail.com>
2025-06-11 13:32:09 -0500

    day5

---
+++ script.js
@@ -0,0 +1,25 @@
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const number = document.getElementById("number").value;
+
+    let isValid = true;
+
+    if (name === "" || /\d/.test(name)) {
+        nameErr.textContent = "Please enter your name properly.";
+        isValid = false;
+    }
+
+    if (email === "" || !email.includes("@") || !email.includes(".")) {
+        emailErr.textContent = "Please enter a valid email address.";
+        isValid = false;
+    }
+
+    if (number === "" ) {
+        passErr.textContent = "Please enter a phone number with your country's dialing code at the start";
+        isValid = false;
+    }
+    else {
+        return false;
+    }
+
+
